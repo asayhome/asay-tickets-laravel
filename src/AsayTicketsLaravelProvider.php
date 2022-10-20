03f216c31152776d20e24970c518ebe4f2b7ba58
Abdelrahman Saeed Elhassan <abdosaeedelhassan@gmail.com>
2022-10-20 08:29:03 +0200

    updates

---
+++ src/AsayTicketsLaravelProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AsayHome\AsayTicketsLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class AsayTicketsLaravelProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        dd('her we are');
+        # code...
+    }
+    public function register()
+    {
+        # code...
+    }
+}
