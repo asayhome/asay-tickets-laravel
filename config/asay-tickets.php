db9f984e6f1bf1c70a512b96a2e35227cf9eedf6
Abdelrahman Saeed Elhassan <abdosaeedelhassan@gmail.com>
2022-11-17 20:57:03 +0200

    updates

--- config/asay-tickets.php
+++ config/asay-tickets.php
@@ -6,5 +6,19 @@ return [
     'userModel' => User::class,
     "userSelectedFileds" => [
         'id', 'first_name', 'last_name'
+    ],
+    'permissions' => [
+        'ticket_section' => 'Ticket section',
+        'ticket_section_add' => 'Add new ticket section',
+        'ticket_section_edit' =>  'Edit ticket section',
+        'ticket_section_show' => 'Show ticket section',
+        'ticket_section_show_all' =>  'Show all tickets sections',
+        'ticket_section_delete' =>  'Delete ticket section',
+        'ticket' =>  'Ticket',
+        'ticket_add' =>  'Add new ticket',
+        'ticket_edit' => 'Edit ticket',
+        'ticket_show' => 'Show ticket',
+        'ticket_show' => 'Show all tickets',
+        'ticket_delete' => 'Delete ticket',
     ]
 ];
